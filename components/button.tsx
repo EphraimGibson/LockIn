4e272febf8c4df38e2fb9e43d429b0c32febabb8
bansds <nosleepleak1@outlook.com>
2025-03-10 19:49:30 +0000

    initial commit to developer branch

---
+++ components/button.tsx
@@ -0,0 +1,16 @@
+import { Text, View, StyleSheet, Pressable } from "react-native";
+
+type Props = { 
+    label : string;
+};
+
+export default function Button ({label} : Props) {
+    return (
+        <view>
+            <Pressable>
+                cle
+            </Pressable>
+
+        </view>
+    );
+}
