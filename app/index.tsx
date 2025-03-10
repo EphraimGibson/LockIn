cb8d5eaa3d9f93c39b1335efd1acaf2b99f9b730
Remy <mrgibs97@gmail.com>
2025-03-10 19:49:30 +0000

    initial commit to developer branch

---
+++ app/index.tsx
@@ -0,0 +1,41 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>TaskUp</Text>
+      <Text style={{marginTop: 20}}>Welcome to TaskUp, streamline your tasks effortlessly!</Text>
+      <Link href="/guest" style={styles.button}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#654321',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: 'lightblue',
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+    borderRadius: 5,
+    marginTop: 50,
+  },
+  buttonText:{
+    color: 'black',
+    fontWeight: 'light',
+    fontSize: 15,
+  }
+});
