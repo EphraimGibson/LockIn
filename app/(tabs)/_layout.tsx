c372450b25ea6e8a984caca66d5c825ffbbf375d
Remy <nosleepleak1@outlook.com>
2025-03-14 02:25:50 +0000

    Added plus button for new task

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,19 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
     return (
-        <Tabs>
+        <Tabs
+        
+        screenOptions={{tabBarActiveTintColor: "black",
+                        tabBarInactiveTintColor: "gray", 
+                       }}>
             <Tabs.Screen
                 name="guest"
                 options={{
                     headerTitle: "Welcome Tasker",
                     title: "Home",
+                    tabBarIcon: ({focused,color}) => <Ionicons name="home" size={24} color="black"></Ionicons>
                 }}
             />
             <Tabs.Screen
