a7622d44e3920cd48faf7df18b9c3cd920f2af26
Remy <nosleepleak1@outlook.com>
2025-03-31 22:20:22 +0100

    AddTask button implemented

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,22 @@
-import { Tabs } from "expo-router";
+import { Tabs, Stack} from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
     return (
         <Tabs
-        
-        screenOptions={{tabBarActiveTintColor: "black",
-                        tabBarInactiveTintColor: "gray", 
-                       }}>
+            screenOptions={{
+                tabBarActiveTintColor: "black",
+                tabBarInactiveTintColor: "gray",
+            }}
+        >
             <Tabs.Screen
                 name="guest"
                 options={{
                     headerTitle: "Welcome Tasker",
                     title: "Home",
-                    tabBarIcon: ({focused,color}) => <Ionicons name="home" size={24} color="black"></Ionicons>
+                    tabBarIcon: ({ focused, color }) => (
+                        <Ionicons name="home" size={24} color={color} />
+                    ),
                 }}
             />
             <Tabs.Screen
