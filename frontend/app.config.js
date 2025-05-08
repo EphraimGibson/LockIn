8c58128c487c08042cc7bc9328616d7085ad4b2c
Ephraim Gibson <mrgibs97@gmail.com>
2025-05-08 15:31:44 +0100

    trting to build

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -3,8 +3,19 @@ import "dotenv/config";
 export default {
   expo: {
     scheme: "LockIn",
+    android: {
+      package: "com.remy.LockIn", // <-- Add this line
+    },
+    "ios": {
+    "bundleIdentifier": "com.remy.LockIn"
+  },
+    
     extra: {
       IP: process.env.IP,
+
+      "eas": {
+        "projectId": "64dfdc15-a6b5-43ad-8fb4-0acb160517fe"
+      }
     },
   },
 };
